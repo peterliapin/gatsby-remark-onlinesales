7cf7f469301f9565811851aff1064c6433970733
UniqueUlysees <boriscool007@gmail.com>
2023-02-28 04:26:06 +0300

    Update to 1.1.0

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const esmRequire = require('./esmRequire')
 const visit = esmRequire('unist-util-visit').visit;
+const fs = require('fs-extra');
+const path = require('path');
+const { createRemoteFileNode  } = require(`gatsby-source-filesystem`)
 
-module.exports.default = ({ markdownAST }, pluginOptions) => {
+module.exports.default = async ({ 
+    markdownAST,  
+    actions,
+    createContentDigest,
+    createNodeId,
+    getCache,
+    cache, 
+}, pluginOptions) => {
+    const processImages = async(node) =>{
+      const imageMatch = node.url.match('/api/images/(.*)/(.*)');
+      if (!imageMatch) {
+          return;
+      }
+      const scope = imageMatch[1];
+      const fileName = imageMatch[2];
+      const nodeId = createNodeId(`RemoteImage-${scope}-${fileName}`);
+      const fileNode = await createRemoteFileNode({
+          url: `${pluginOptions.url}${node.url}`,
+          parentNodeId: nodeId,
+          getCache,
+          createNode,
+          createNodeId,
+          cache,
+        });
+        const outputPath = path.join(process.cwd(), 'public', 'static', scope);
+        const filePath = path.join(outputPath, fileName);
+        fs.ensureDirSync(outputPath);
+        fs.copy(fileNode.absolutePath, filePath, (err) => {
+          if (err) {
+            console.error(
+              `error copying file from ${fileNode.absolutePath} to ${filePath}`,
+              err,
+            );
+          }
+        });
+        const readyPath = `/static/${scope}/${fileName}`;
+        console.log(`Created ${fileName} file at ${readyPath}`);
+        node.url = readyPath;
+      }
+    const { createNode } = actions
+    const imageNodes = [];
     visit(markdownAST, (node) => {
         if (node.type !== 'image') {
             return;
         }
-        node.url = `${pluginOptions.url}${node.url}`;
+        imageNodes.push(node);
     })
-  }
-    
+    const promises = imageNodes.map((node) => {
+      return processImages(node);
+    });
+    await Promise.all(promises);
+}
